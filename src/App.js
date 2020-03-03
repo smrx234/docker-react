867fc60df0b876d0f17dc27b12b4f42d6113b4e7
Kushal <connect.kushal@gmail.com>
2020-03-03 22:03:17 +0530

    update npm run test and edit app.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          test app        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
